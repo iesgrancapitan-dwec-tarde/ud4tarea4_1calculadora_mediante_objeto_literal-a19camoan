8e48713b6edba8b07be6b0e800e2734329ad0a0b
a19camoan <56250607+a19camoan@users.noreply.github.com>
2023-02-12 23:37:36 +0100

    falla por culpa del separador de millares

--- js/main.js
+++ js/main.js
@@ -22,6 +22,9 @@
  * @author Andrés Castillero Moriana
  */
 {
+    /**
+     * Añade el estilo css a la página.
+     */
     const meter_css = () => {
         const link = document.createElement("link");
         link.rel = "stylesheet";
@@ -30,26 +33,34 @@
         document.head.appendChild(link);
     };
 
-
     const calculadora = {
-        // Usamos los separadores de millares y decimales anglosajones. Se pueden cambiar por otros.
-        separador_millares: ",",
-        separador_decimal: ".",
+        // Se pueden cambiar.
+        separador_millares: ".",
+        separador_decimal: ",",
         // El valor inicial de la pantalla es "0".
         pantalla: "0",
+        // Valor real de la pantalla. Se usa para las operaciones.
+        valor_real: 0,
 
         /**
          * Comprueba el botón que se ha pulsado y ejecuta la acción correspondiente.
          * 
-         * @param {string} boton el botón que se ha pulsado
+         * @param {string} boton el botón que se ha pulsado.
          */
         comprobar_boton: function (boton) {
             switch (boton) {
+                case "+":
+                case "-":
+                case "X":
+                case "÷":
+                case "%":
+                case "=":
+                    break;
                 case ",":
                     this.agregar_separador_decimal();
                     break;
                 case "CE":
-                    this.pantalla = "0";
+                    this.resetear_calculadora();
                     break;
                 case "±":
                     this.cambiar_signo();
@@ -71,34 +82,43 @@
                 this.pantalla = "0" + this.separador_decimal;
             else if (!this.pantalla.includes(this.separador_decimal))
                 this.pantalla = this.pantalla + this.separador_decimal;
+
+            this.valor_real = parseFloat(this.pantalla.replace(this.separador_decimal, "."));
         },
 
         /**
          * Cambia el signo del número que se está escribiendo en la calculadora.
          */
         cambiar_signo: function () {
-            if (!this.pantalla == "0")
-                this.pantalla = this.pantalla * -1;
+            if (!this.pantalla == "0") {
+                this.valor_real = -this.valor_real;
+                this.pantalla = this.valor_real.toString().replace(".", this.separador_decimal);
+            }
         },
 
         /**
          * Borra el último número introducido en la calculadora.
          */
         borrar_ultimo_caracter: function () {
-            if (this.pantalla.length === 1)
+            if (this.pantalla.length === 1) {
                 this.pantalla = "0";
-            else
-                this.pantalla = this.pantalla.substring(0, this.pantalla.length - 1);
+                this.valor_real = 0;
+            } else {
+                this.valor_real = parseFloat(this.pantalla.replace(this.separador_decimal, ".").substring(0, this.pantalla.length - 1));
+                this.pantalla = this.valor_real.toString().replace(".", this.separador_decimal);
+            }
         },
 
         /**
          * Añade un número a la calculadora.
          * 
-         * @param {string} numero el número que se quiere añadir a la calculadora
+         * @param {string} numero el número que se quiere añadir a la calculadora.
          */
         agregar_numero: function (numero) {
-            if (this.pantalla == "0")
+            if (this.pantalla == "0") {
                 this.pantalla = numero;
+                this.valor_real = parseFloat(numero);
+            }
             else {
                 this.agregar_separador_millar(numero);
             };
@@ -112,14 +132,25 @@
         agregar_separador_millar: function (numero) {
             const patron = new RegExp("\\" + this.separador_millares, "g");
 
-            if ((this.pantalla.replace(patron, "").length % 3 === 0) && !this.pantalla.includes(this.separador_decimal))
+            if ((this.pantalla.replace(patron, "").length % 3 === 0) && !this.pantalla.includes(this.separador_decimal)) {
                 this.pantalla = this.pantalla + this.separador_millares + numero;
-            else
+                this.valor_real = parseFloat(this.pantalla.replace(this.separador_decimal, ".").replace(patron, ""));
+            } else {
                 this.pantalla = this.pantalla + numero;
+                this.valor_real = parseFloat(this.pantalla.replace(this.separador_decimal, "."));
+            }
         },
 
         /**
-         * Dibuja la calculadora en la página
+         * Resetea la calculadora.
+         */
+        resetear_calculadora: function () {
+            this.pantalla = "0";
+            this.valor_real = 0;
+        },
+
+        /**
+         * Dibuja la calculadora en la página.
          */
         dibujar_calculadora: function () {
             const fragment = document.createDocumentFragment();
@@ -130,31 +161,42 @@
             input.setAttribute("readonly", "readonly");
             fragment.appendChild(input);
 
-            fragment.appendChild(document.createElement("br"));
-
             const botones = ["CE", "←", "%", "+", 7, 8, 9, "-", 4, 5, 6, "X", 1, 2, 3, "÷", 0, "±", ",", "="];
+
+            const boton_container = document.createElement("div");
             botones.forEach((element, index) => {
                 const boton = document.createElement("button");
                 boton.textContent = element;
-                fragment.appendChild(boton);
+                boton_container.appendChild(boton);
 
                 if ((index + 1) % 4 === 0)
-                    fragment.appendChild(document.createElement("br"));
+                    boton_container.appendChild(document.createElement("br"));
             });
 
+            fragment.appendChild(boton_container);
             document.body.appendChild(fragment);
+        },
+
+        /**
+         * Inicializa la calculadora.
+         */
+        init: function () {
+            this.dibujar_calculadora();
+
+            // Mediante delegación de eventos le damos funcionalidad a los botones.
+            const botones = document.querySelectorAll("button");
+            const input = document.querySelector("input");
+            botones.forEach((boton) => {
+                boton.addEventListener("click", () => {
+                    this.comprobar_boton(boton.textContent);
+                    input.value = this.pantalla;
+                });
+            });
         }
     };
 
     document.addEventListener("DOMContentLoaded", () => {
         meter_css();
-        calculadora.dibujar_calculadora();
-
-        document.querySelectorAll("button").forEach((boton) => {
-            boton.addEventListener("click", () => {
-                calculadora.comprobar_boton(boton.textContent);
-                document.querySelector("input").value = calculadora.pantalla;
-            });
-        });
+        calculadora.init();
     });
 }
